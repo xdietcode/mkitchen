2f17478f75b5d11d9ebc9eaa98d53704fda80b4e
xdietcode <xdietcode@gmail.com>
2021-01-23 23:14:12 -0800

    add JPA annotation for Ingredient

--- src/main/java/com/mkitchen/server/entity/Ingredient.java
+++ src/main/java/com/mkitchen/server/entity/Ingredient.java
@@ -1,24 +1,34 @@
 package com.mkitchen.server.entity;
 
-import lombok.Builder;
-import lombok.Value;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 import javax.persistence.*;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
-@Value
-@Builder
+@Data
+@NoArgsConstructor
 @Entity
+@Table(name = "ingredients")
 public class Ingredient {
 
     @Id
-    @GeneratedValue
-    private int id;
+    @Column(name = "ingredient_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    @Column(length = 20)
     private String name;
-
-    @OneToMany(mappedBy = "ingredient", cascade = CascadeType.ALL)
-    private List<RecipeIngredient> recipeIngredients;
-
     private String url;
+
+    @OneToMany(mappedBy = "ingredient")
+    private Set<RecipeIngredient> recipeIngredients = new HashSet<>();
+
+    public Ingredient(Integer id, String name, String url) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
 }
