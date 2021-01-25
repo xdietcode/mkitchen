ce04c52e9a40c5cca31b7bf84f4147d1e122eed9
xdietcode <xdietcode@gmail.com>
2021-01-25 12:41:37 -0800

    add findByName

--- src/main/java/com/mkitchen/server/repository/AmazonUrlRepository.java
+++ src/main/java/com/mkitchen/server/repository/AmazonUrlRepository.java
@@ -2,6 +2,10 @@ package com.mkitchen.server.repository;
 
 import com.mkitchen.server.entity.AmazonUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AmazonUrlRepository extends JpaRepository<AmazonUrl, String> {
+
+    @Query("SELECT a FROM AmazonUrl a WHERE a.ingredientName = ?1")
+    AmazonUrl findByName(String name);
 }
