b838621778a743457d1bd3f0b352d42e67fbe19b
xdietcode <xdietcode@gmail.com>
2021-02-04 14:54:46 -0800

    create MyUserDetails

---
+++ src/main/java/model/MyUserDetails.java
@@ -0,0 +1,66 @@
+package model;
+
+import com.mkitchen.server.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private List<GrantedAuthority> authorities;
+
+    public MyUserDetails(User user) {
+        this.userName = user.getUserName();
+        this.password = user.getPassword();
+        this.enabled = user.isEnabled();
+        this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getRole()));
+
+//        this.authorities = Arrays.stream(user.getRole().split(","))
+//                .map(SimpleGrantedAuthority::new)
+//                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+
+}
