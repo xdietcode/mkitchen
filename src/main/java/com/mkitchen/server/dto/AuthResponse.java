33210fc440a31ef3dda065d36fb060a0577f3c3b
xdietcode <xdietcode@gmail.com>
2021-02-04 14:17:24 -0800

    add dto for AuthRequest and AuthResponse

---
+++ src/main/java/com/mkitchen/server/dto/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.mkitchen.server.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse implements Serializable {
+
+    private String jwt;
+
+}
