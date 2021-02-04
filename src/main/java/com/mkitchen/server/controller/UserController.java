6e2ace6b8d53490136a4fc2254d501e0a48c61d7
xdietcode <xdietcode@gmail.com>
2021-02-04 13:02:59 -0800

    create UserController

---
+++ src/main/java/com/mkitchen/server/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.mkitchen.server.controller;
+
+import com.mkitchen.server.dto.RegisterRequest;
+import com.mkitchen.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(maxAge = 3600)
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    private ResponseEntity<?> addRecipe(@RequestBody RegisterRequest request) {
+        return userService.save(request.getUsername(), request.getPassword());
+    }
+}
