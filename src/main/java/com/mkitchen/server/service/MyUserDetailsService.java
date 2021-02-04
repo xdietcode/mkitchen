b3907cfb90d9626753a50540862a787eb9b4a264
xdietcode <xdietcode@gmail.com>
2021-02-04 14:55:34 -0800

    create MyUserDetailsService

---
+++ src/main/java/com/mkitchen/server/service/MyUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.mkitchen.server.service;
+
+import com.mkitchen.server.entity.User;
+import com.mkitchen.server.repository.UserRepository;
+import model.MyUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public MyUserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("No user found: " + userName));
+        return user.map(MyUserDetails::new).get();
+    }
+}
