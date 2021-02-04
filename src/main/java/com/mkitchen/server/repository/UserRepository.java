06e27f19abb8a56ee0ec2a114e149f4c00281340
xdietcode <xdietcode@gmail.com>
2021-02-04 14:53:58 -0800

    change return type to Optional

--- src/main/java/com/mkitchen/server/repository/UserRepository.java
+++ src/main/java/com/mkitchen/server/repository/UserRepository.java
@@ -4,8 +4,10 @@ import com.mkitchen.server.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u WHERE u.userName = ?1")
-    User findByName(String name);
+    Optional<User> findByName(String name);
 }
